9e0662277475c6cfda30bd4d5828d4e7f456e9c7
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-04-09 15:29:39 -0300

    feat: add navigation inital configurations + logo + hamburguer + menu dropdown, etc...
    next steps: add new font and better responsiveness in navigation drop-down

--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useMetadataStore from "../../stores/metadata.store";
 import "./login.style.css";
 
@@ -5,6 +6,12 @@ function LoginPage() {
   const languages = useMetadataStore((state) => state.languages);
   const currentLanguage = useMetadataStore((state) => state.currentLanguage);
   console.log(languages, currentLanguage);
+
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+
+  const openMenu = () => {
+    setDropdownMenu(!dropdownMenu);
+  };
 
   return (
     <main class="login-container">
@@ -16,18 +23,77 @@ function LoginPage() {
         />
       </div>
 
-      <nav class="navigation-container">
-        <header class="navigation-header">
+      <header class="header-container">
+        <nav class="navigation-container">
           {/* Logo */}
-          <div></div>
+          <div class="logo">
+            <img
+              id="navigation-logo"
+              src="https://storage.123fakturera.se/public/icons/diamond.png"
+              alt="123Fakturera"
+            />
+          </div>
 
           {/* Hambuguer Button */}
-          <div></div>
+          <div class="hamburguer-container">
+            <div class="navigation-hamburguer" onClick={openMenu}>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 24 24"
+                class="navigation-svg"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+              </svg>
+            </div>
+
+            {/* Dropdown Menu */}
+            <div
+              class="navigation-drop-down"
+              style={{
+                height: dropdownMenu ? "325px" : "0px",
+              }}
+            >
+              <div class="navigation-drop-down-items">
+                <a class="menu-item" href="">
+                  home
+                </a>
+                <a class="menu-item" href="">
+                  order
+                </a>
+                <a class="menu-item" href="">
+                  our costumers
+                </a>
+                <a class="menu-item" href="">
+                  about us
+                </a>
+                <a class="menu-item" href="">
+                  contact us
+                </a>
+              </div>
+            </div>
+          </div>
 
           {/* All Navbar options */}
-          <div></div>
-        </header>
-      </nav>
+          <div class="navigation-options">
+            {/* Pc normal navbar */}
+            <div class="navigation-pc">
+              <span class="navigation-pc-items">home</span>
+              <span class="navigation-pc-items">order</span>
+              <span class="navigation-pc-items">our costumers</span>
+              <span class="navigation-pc-items">about us</span>
+              <span class="navigation-pc-items">contact us</span>
+            </div>
+
+            {/* Language selector */}
+            <div class="navigation-language-select"></div>
+          </div>
+        </nav>
+      </header>
 
       <section class="main-content">
         {/* Login Form */}
